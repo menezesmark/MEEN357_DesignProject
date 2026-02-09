6b1476457e71c3d16176ab06025130d58413b1c9
Mark Menezes <72058369+menezesmark@users.noreply.github.com>
2026-02-09 02:31:09 -0600

    updated

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -4,42 +4,40 @@ from subfunctions import *
 
 Crr = 0.15
 
-slope_array_deg = np.linspace(-75, 75, 1000)
+slope_array_deg = np.linspace(-15, 35, 25)
 v_max = np.zeros(len(slope_array_deg))
 
-# conv omega to rover velo
-def omega_to_velocity(omega, rover):
-    radius = rover['wheel_assembly']['wheel']['radius']
-    ng = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
-    return radius * omega / ng
+ng = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
+radius = rover['wheel_assembly']['wheel']['radius']
 
-# max velo for each slope
+
 for i in range(len(slope_array_deg)):
     angle = slope_array_deg[i]
     
-    omega_low = 0
-    omega_high = motor['speed_noload']
+    # bicstion rang
+    a = 0.0
+    b = motor['speed_noload']
     
-    tol = 1e-5 
-    max_iter = 100
+    tol = 1e-6
+    max_iter = 1000
+    
+    f_start = F_net(a, angle, rover, planet, Crr)
     
     for q in range(max_iter):
-        omega_mid = (omega_low + omega_high) / 2
+        c = (a + b) / 2
+        f_c = F_net(c, angle, rover, planet, Crr)
         
-        # net f @ midpoint
-        f_mid = F_net(omega_mid, angle, rover, planet, Crr)
-        
-        if f_mid == 0 or (omega_high - omega_low) / 2 < tol: #check if close
+        if f_c == 0 or (b - a) / 2 < tol:
+            omega_eq = c
             break
         
-        f_low = F_net(omega_low, angle, rover, planet, Crr)
-        
-        if np.sign(f_mid) == np.sign(f_low):
-            omega_low = omega_mid
+        if np.sign(f_c) == np.sign(f_start):
+            a = c
         else:
-            omega_high = omega_mid
-            
-    v_max[i] = omega_to_velocity(omega_mid, rover)
+            b = c
+    
+
+    v_max[i] = (omega_eq / ng) * radius
 
 plt.plot(slope_array_deg, v_max)
 plt.title("Max Velocity vs. Terrain Slope")
@@ -47,4 +45,3 @@ plt.xlabel("Terrain Angle [deg]")
 plt.ylabel("Max Rover Velocity [m/s]")
 
 plt.show()
-
