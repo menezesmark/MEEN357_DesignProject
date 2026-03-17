57e34e2229855d1266c3fc4cdc9c67fe6d256f1e
Mark Menezes <72058369+menezesmark@users.noreply.github.com>
2026-03-17 11:51:26 -0500

    made

--- experiment_visualization.py
+++ experiment_visualization.py
@@ -1,1 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from subfunctions import *
+import scipy.interpolate as inp
 
+alpha_dist = experiment['alpha_dist']
+alpha_deg = experiment['alpha_deg']
+
+alpha_fun = inp.interp1d(alpha_dist, alpha_deg, kind = 'cubic', fill_value= 'extrapolate') #fit the cubic spline
+
+x_eval = np.linspace(np.min(alpha_dist), np.max(alpha_dist), 100)
+alpha_eval = alpha_fun(x_eval)
+
+plt.figure()
+plt.plot(alpha_dist, alpha_deg, '*')
+plt.plot(x_eval, alpha_eval, '-', label='Interpolation')
+
+
+plt.xlabel('Distance [m]')
+plt.ylabel('Angle [degree]')
+plt.title('Terrain Angle vs. Distance')
+plt.legend()
+plt.show()
