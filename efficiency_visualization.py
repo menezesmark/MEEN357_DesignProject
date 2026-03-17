0cc86fa4ba0287edac8ba72dca0ad7ec8272f867
Mark Menezes <72058369+menezesmark@users.noreply.github.com>
2026-03-17 12:19:33 -0500

    created eff vis but didnt finish

--- efficiency_visualization.py
+++ efficiency_visualization.py
@@ -1,1 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:09:44 2026
 
+@author: Mark
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from subfunctions import *
+import scipy.interpolate as inp
+
+effcy_tau = motor['effcy_tau']
+effcy = motor['effcy']
+
+
+
+effcy_fun = inp.interp1d(effcy_tau, effcy, kind = 'cubic') # fit the cubic spline
+
+x_eval = np.linspace(motor['torque_noload'], motor['torque_stall'], 100)
+effcy_eval = effcy_fun(x_eval)
