e53079144e9b8139859d2d3414b07499218f573d
Esteban-de-Leon <deleon.esteban@outlook.com>
2026-03-18 22:12:41 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -287,6 +287,7 @@ def battenergy(t, v, rover): # calc total energy used over time-velo pair/ = E
         if t.size != v.size:
             raise Exception("t and v must be the same size")
     
+    omega = motorW(v, rover)
     tau = tau_dcmotor(omega, motor)
     P = mechpower(v, rover)
 
