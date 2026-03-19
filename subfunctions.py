19dc0f95e59ca98e79adff1eb2f1152a984bdce4
Esteban-de-Leon <deleon.esteban@outlook.com>
2026-03-18 22:21:54 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -291,11 +291,7 @@ def battenergy(t, v, rover): # calc total energy used over time-velo pair/ = E
     tau = tau_dcmotor(omega, motor)
     P = mechpower(v, rover)
 
-    alpha_fun = inp.interp1d(motor['effcy_tau'], 
-                             motor['effcy'], 
-                             kind='cubic',
-                             bounds_error=False,
-                             fill_value='extrapolate')
+    effcy_fun = interp1d(effcy_tau, effcy, kind = 'cubic') # fit the cubic spline
 
     eta = alpha_fun(tau)
     Pbatt = P/eta
