d17b26758bd65ed773c7494f1a784d6b52d4567e
Mark Menezes <72058369+menezesmark@users.noreply.github.com>
2026-03-18 20:15:39 -0500

    Refactor efficiency visualization with motor data
    Updated efficiency visualization to use motor data from rover and improved interpolation handling.

--- efficiency_visualization.py
+++ efficiency_visualization.py
@@ -4,18 +4,30 @@ Created on Tue Mar 17 12:09:44 2026
 
 @author: Mark
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
-from subfunctions import *
 import scipy.interpolate as inp
-
-effcy_tau = motor['effcy_tau']
-effcy = motor['effcy']
+from subfunctions import *
 
 
+motor = rover['wheel_assembly']['motor']
 
-effcy_fun = inp.interp1d(effcy_tau, effcy, kind = 'cubic') # fit the cubic spline
+effcy_tau = np.array(motor['effcy_tau'])
+effcy = np.array(motor['effcy'])
 
-x_eval = np.linspace(motor['torque_noload'], motor['torque_stall'], 100)
+
+effcy_fun = inp.interp1d(effcy_tau,effcy,kind='cubic',fill_value='extrapolate')
+
+x_eval = np.linspace(np.min(effcy_tau), np.max(effcy_tau), 100)
 effcy_eval = effcy_fun(x_eval)
+
+plt.figure()
+plt.plot(effcy_tau, effcy, '*')
+plt.plot(x_eval, effcy_eval, '-', label='Interpolation')
+plt.xlabel('Torque [Nm]')
+plt.ylabel('Efficiency')
+plt.title('Motor Efficiency vs. Torque')
+
+plt.legend()
+plt.grid(True)
+plt.show()
